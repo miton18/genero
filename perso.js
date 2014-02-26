57c0aae50e6605c8a63f8be71d06d4da483ba47d
miton18 <rcd18@hotmail.fr>
2014-02-26 13:04:45 +0100

    gradient

--- perso.js
+++ perso.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
 
     $( "#tabs" ).tabs();
+    $( "#tabs" ).tabs( "option", "hide", { effect: "fold", duration: 1000 } );
+    $( "#tabs" ).tabs( "option", "show", { effect: "fold", duration: 1000 } );
 });
