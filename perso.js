fac8be1a9b7372dd96e79f7b6fd09c260146f98c
miton18 <rcd18@hotmail.fr>
2014-02-28 10:33:07 +0100

    probleme colorpicker qui ne met pas a jour ng-model

--- perso.js
+++ perso.js
@@ -4,7 +4,14 @@ $(document).ready(function() {
     $( "#tabs" ).tabs( "option", "hide", { effect: "fold", duration: 300 } );
     $( "#tabs" ).tabs( "option", "show", { effect: "fold", duration: 300 } );
 
-    $(".color").spectrum({
-   	 color: "#f00"
+    $('.color').colpick({
+		colorScheme:'dark',
+		layout:'rgbhex',
+		color:'ff8800',
+		onSubmit:function(hsb,hex,rgb,el) {
+			$(el).css('background-color', '#'+hex);
+			$(el).val('#' + hex );
+			$(el).colpickHide();
+		}
 	});
 });
