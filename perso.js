85867b4fd43c6ec48d364a06f883214610cc4f10
miton <rcd18@hotmail.fr>
2014-02-28 07:39:31 +0100

    colorpicker

--- perso.js
+++ perso.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
     $( "#tabs" ).tabs();
-    $( "#tabs" ).tabs( "option", "hide", { effect: "fold", duration: 1000 } );
-    $( "#tabs" ).tabs( "option", "show", { effect: "fold", duration: 1000 } );
+    $( "#tabs" ).tabs( "option", "hide", { effect: "fold", duration: 300 } );
+    $( "#tabs" ).tabs( "option", "show", { effect: "fold", duration: 300 } );
+
+    $(".color").spectrum({
+   	 color: "#f00"
+	});
 });
